5c358ee06a23e152c724e3c5ac0a8f98a8c5ad2e
Henk van Kampen <henkvk@gmail.com>
2025-10-26 16:48:34 +0100

    initial commit

---
+++ src/main/java/com/hbvk/App.java
@@ -0,0 +1,7 @@
+package com.hbvk;
+
+public class App {
+    static void main() {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/test/java/com/hbvk/AppTest.java
@@ -0,0 +1,21 @@
+package com.hbvk;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
